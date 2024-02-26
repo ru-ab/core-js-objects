ba083c820df19a6d139e61da4c1a3c33e1d6337a
ru-ab <ruabdrakhmanov@gmail.com>
2024-02-26 17:50:32 +0600

    feat: implement sellTickets function

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -152,8 +152,16 @@ function makeWord(lettersObject) {
  *    sellTickets([25, 25, 50]) => true
  *    sellTickets([25, 100]) => false (The seller does not have enough money to give change.)
  */
-function sellTickets(/* queue */) {
-  throw new Error('Not implemented');
+function sellTickets(queue) {
+  let sellerMoney = 0;
+  for (let i = 0; i < queue.length; i += 1) {
+    sellerMoney += 50 - queue[i];
+    if (sellerMoney < 0) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
 /**
