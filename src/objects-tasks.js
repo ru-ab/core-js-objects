093c19ff3071754b6f2f1f639902d17a276e1f82
ru-ab <ruabdrakhmanov@gmail.com>
2024-02-26 14:51:15 +0600

    feat: implement mergeObjects function

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -33,8 +33,14 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  const result = {};
+  objects.forEach((obj) => {
+    Object.entries(obj).forEach(([key, value]) => {
+      result[key] = result[key] ? result[key] + value : value;
+    });
+  });
+  return result;
 }
 
 /**
