68f602b6fe310d058022aa1d585693003b07bc71
ru-ab <ruabdrakhmanov@gmail.com>
2024-02-26 17:19:53 +0600

    feat: implement removeProperties function

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -56,8 +56,10 @@ function mergeObjects(objects) {
  *    removeProperties({name: 'John', age: 30, city: 'New York'}, 'age') => {name: 'John', city: 'New York'}
  *
  */
-function removeProperties(/* obj, keys */) {
-  throw new Error('Not implemented');
+function removeProperties(obj, keys) {
+  const copy = { ...obj };
+  keys.forEach((key) => delete copy[key]);
+  return copy;
 }
 
 /**
